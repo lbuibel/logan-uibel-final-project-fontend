4f270203d706cd8f61d2d7e92de41fb5b4dc98b1
Logan Uibel <lbuibel@gmail.com>
2020-03-10 11:15:13 -0600

    setup route names

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,32 @@
   <v-container>
    <h1>Routes</h1>
    <hr>
+
+       <!-- Apollo watched Graphql query -->
+    <ApolloQuery
+      :query="require('../graphql/HelloWorld.gql')"
+    >
+      <template v-slot="{ result: { loading, error, data } }">
+        <!-- Loading -->
+        <div v-if="loading" class="loading apollo">Loading...</div>
+
+        <!-- Error -->
+        <div v-else-if="error" class="error apollo">An error occured</div>
+
+        <!-- Result -->
+        <div v-else-if="data" class="result apollo">{{ data.Routes }}
+          <v-list>
+            <v-list-item v-for="(route, i) in data.Routes" :key="i">
+              <p> {{ route.name }}</p>
+            </v-list-item>
+          </v-list>
+        </div>
+
+        <!-- No result -->
+        <div v-else class="no-result apollo">No result :(</div>
+      </template>
+    </ApolloQuery>
+
   </v-container>
 </template>
 
