c4a08a637cc6438b0d00fcf8a5ff8369c911250f
Logan Uibel <lbuibel@gmail.com>
2020-04-01 13:00:58 -0600

    setup form with edited iframe input

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -17,6 +17,40 @@
         <!-- Form here -->
         <v-form>
             <v-container>
+                <v-row>
+                    <v-col cols="12" sm="8" md="6">
+                        <v-text-field v-model="name" label="Name" required></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12" sm="8" md="6">
+                        <v-select
+                        v-model="type"
+                        label="Route Type"
+                        :items="types"
+                        >
+                        </v-select>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12" sm="8" md="6">
+                        <v-text-field v-model.number="miles" label="Miles" required></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="6" sm="4" md="3">
+                        <v-text-field v-model.number="startingElevation" label="Starting Elevation" required></v-text-field>
+                    </v-col>
+                    <v-col cols="6" sm="4" md="3">
+                        <v-text-field v-model.number="finalElevation" label="Final Elevation" required></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12" sm="8" md="6">
+                        <v-text-field v-model="iframeData" label="iframe" required @change="iframeEdit()"></v-text-field>
+                    </v-col>
+                </v-row>
+
                 <v-btn large primary :disabled="loading" @click="mutate()">Add Route</v-btn>
                 <p v-if="error">An Error occured: {{ error }}</p>
             </v-container>
@@ -30,19 +64,25 @@ export default {
     name: 'AddRoute',
     data: () => {
         return {
-            name: 'vue test',
-            type: 'paved',
-            miles: 25,
-            startPoint: 'nowhere',
-            endPoint: 'somewhere',
-            startingElevation: 100,
-            finalElevation: 200,
-            iframeData: 'iframe',
+            name: '',
+            type: '',
+            miles: 0,
+            startPoint: '123',
+            endPoint: '456',
+            startingElevation: 0,
+            finalElevation: 0,
+            iframeData: '',
+            types: ['paved', 'singletrack', 'gravel']
         }
     },
     methods: {
         onDone() {
             return console.log('Done')
+        },
+        iframeEdit() {
+            const width = this.iframeData.indexOf('width')
+            const height = this.iframeData.indexOf('height')
+            return String(this.iframeData = (this.iframeData.slice(13, width) + this.iframeData.slice(height + 13).slice(0,-10)))
         }
     }
 }
@@ -51,3 +91,4 @@ export default {
 <style lang="less" scoped>
 
 </style>
+)
