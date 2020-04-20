bee353bab736132b2ac612e261a5685566b3ded9
Logan Uibel <lbuibel@gmail.com>
2020-04-20 12:30:49 -0600

    setup rest search by miles

--- src/components/TopRoutes.vue
+++ src/components/TopRoutes.vue
@@ -1,16 +1,25 @@
 <template>
   <v-container>
 
-
     <v-row align="center">
       <v-col class="d-flex" cols="12" sm="6">
         <v-select
           :items="items"
-          label="Select Max Average Gradient"
-          outlined
+          label="Max Average Gradient"
           v-model="grade"
           @change="getRoutesByGrade()"
         ></v-select>
+      </v-col>
+    </v-row>
+
+    <v-row align="center">
+      <v-col cols="12">
+        <v-subheader class="pl-0">Max Miles</v-subheader>
+        <v-slider
+          v-model="miles"
+          thumb-label
+          @change="getRoutesByMiles()"
+        ></v-slider>
       </v-col>
     </v-row>
 
@@ -23,26 +32,22 @@
 
       <section v-else>
         <section v-if="dataEmpty">
-          <p>No routes found with an average grade less than {{ grade }}</p>
+          <p>No routes found</p>
         </section>
 
-      <v-row class="flex-wrap">
+          <v-row class="flex-wrap">
               <v-col col="12" xs="12" sm="12"
               v-for="route in gradeRoutes"
                :key="route._id"
                >
                 <div class="large-card">
                   <div class="toproute-info">
-                    <h2> {{route.name}}</h2>
+                    <h4> {{route.name}}</h4>
                   </div>
                 </div>
               </v-col>
           </v-row>
-
       </section>
-
- 
-
 
   </v-container>
 </template>
@@ -55,8 +60,11 @@ import axios from 'axios'
       items: [1,2,3,4,5,6,7,8,9,10],
       grade: null,
       gradeRoutes: null,
+      milesRoutes: null,
       dataEmpty: false,
       error: false,
+
+      miles: null,
     }),
     methods: {
         getRoutesByGrade() {
@@ -66,7 +74,22 @@ import axios from 'axios'
             console.log(response.data)
             if(response.data <= 0){
               this.dataEmpty = true
-              this.gradeRoutes = []
+              this.miles = 0
+              console.log('data empty')
+            } else {
+              this.dataEmpty = false
+              this.gradeRoutes = response.data
+            }
+        }).catch(error => console.log(error))
+    },
+    getRoutesByMiles() {
+        const url = `https://bike-routes-api.herokuapp.com/course/maxMiles/` + this.miles;
+        axios.get(url)
+        .then(response => {
+            console.log(response.data)
+            if(response.data <= 0){
+              this.dataEmpty = true
+              this.grade = 0
               console.log('data empty')
             } else {
               this.dataEmpty = false
@@ -74,6 +97,7 @@ import axios from 'axios'
             }
         }).catch(error => console.log(error))
         }
-    },
+    }
   }
+
 </script>
